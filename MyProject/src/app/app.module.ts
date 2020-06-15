a21e97948c6a450ce2b8aa9c1ddc727693a360ff
Pragati-3 <pragati6630@gmail.com>
2020-06-15 17:04:19 +0530

    UpdateFile

--- MyProject/src/app/app.module.ts
+++ MyProject/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BannerTwoComponent } from './layouts/banner-two/banner-two.component';
+import { BannerOneComponent } from './layouts/banner-one/banner-one.component';
+import { BannerThreeComponent } from './layouts/banner-three/banner-three.component';
+import { ContactUsComponent } from './layouts/contact-us/contact-us.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { NavComponent } from './layouts/nav/nav.component';
+import { ProductsComponent } from './layouts/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerTwoComponent,
+    BannerOneComponent,
+    BannerThreeComponent,
+    ContactUsComponent,
+    FooterComponent,
+    NavComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
---
+++ MyProject/src/app/layouts/banner-one/banner-one.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-banner-one",
+  templateUrl: "./banner-one.component.html",
+  styleUrls: [],
+})
+export class BannerOneComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
